b43060e7e834a487c8a743c3c6df69597ffc1e89
AAAlessandroP <noreply>
2022-12-25 10:03:52 +0100

    preproc

--- esperimenti/filtraggio/ProveEnhancing.m
+++ esperimenti/filtraggio/ProveEnhancing.m
@@ -1,50 +1,31 @@
 clear;
 close all;
+input_dir = 'Progetto/data/unoperpic';
+filenames = dir(fullfile(input_dir, '*.jpg'));
+images = [];
 
-[images, l] = readlists();
-n = numel(images);
+for n = 1:1%length(filenames)/4
+    filename = fullfile(input_dir, filenames(n).name);
+    ycbcr = rgb2ycbcr(imread(filename));
+    imf = medfilt2(imadjust(ycbcr(:,:,1),[0.05, 0.95]), [5 5]);
+    imf = imfilter(imf, fspecial("gaussian", 7, 1.2));
 
-for j = 1 : 1 %PER OGNI PIC 
-    im = im2double(rgb2gray(imread(strcat("Progetto/data/",images{j}))));%attento a pwd
+    bw=1-imbinarize(imf,adaptthresh(imf,0.5,"ForegroundPolarity","dark"));
+    subplot(3,1,1), imshowpair(ycbcr2rgb(ycbcr), bw, "montage"), title("Y");
 
-    
-    
-    imf = medfilt2(imadjust(im,[0.05, 0.95]), [5 5]);
-    imf = imfilter(im, fspecial("gaussian", 7, 1.2));
-    %figure, imshowpair(im, imf,'montage'), title("raw vs. median+gaussian filter");
-    
+        imf = medfilt2(imadjust(ycbcr(:,:,2),[0.05, 0.95]), [5 5]);
+    imf = imfilter(imf, fspecial("gaussian", 7, 1.2));
 
-    otsuRaw = im2bw(im, graythresh(im));
-    otsuProcessed = im2bw(imf, graythresh(imf));
-    %figure, imshowpair(otsuRaw, otsuProcessed,'montage'), title("raw vs. median+gaussian filter: otsu");
+    bw=imbinarize(imf,adaptthresh(imf,0.5,"ForegroundPolarity","bright"));
+    subplot(3,1,2), imshowpair(ycbcr2rgb(ycbcr), bw, "montage") , title("Cb");
 
-    obj.pics = {im, imf};
-    obj.f = {@graythresh, @graythresh};
-    obj.a = {im,'|', imf};
-    runAndDispay(obj);%ok è poco scalabile: se io voglio imbinarize(im, 0.42*ostu(im)) creo una funz apposta?
+            imf = medfilt2(imadjust(ycbcr(:,:,3),[0.05, 0.95]), [5 5]);
+    imf = imfilter(imf, fspecial("gaussian", 7, 1.2));
 
-    
-
-
-    % N=30;
-    % M=15;
-    % for i = 1:size(im,1)-(N-1)
-    %     for j = 1:size(im,2)-(M-1)
-    %         window = im(i:i+(N-1), j:j+(M-1));
-    %         im(i,j) = abs(im(i,j)) < mean(window,"all")+0.05;
-    %     end
-    % end
-    
-    23                                                                                    
-    sav = sauvola(im, [60 60]);
-    savf = sauvola(imf, [60 60]);
-    figure, imshowpair(sav, savf, 'montage'), title('raw vs. median+gaussian filter: sauvola');
-
-%T is a numeric array, then imbinarize interprets T as a locally adaptive threshold.
-    adaptive = imbinarize(im,adaptthresh(im,0.9,"NeighborhoodSize",[97 129]));
-    adaptiveF = imbinarize(imf,adaptthresh(imf));
-    figure, imshowpair(sav, savf, 'montage'), title('raw vs. median+gaussian filter: adaptive');
-
+    bw=imbinarize(imf,adaptthresh(imf,0.5,"ForegroundPolarity","bright"));
+    subplot(3,1,3), imshowpair(ycbcr2rgb(ycbcr), bw, "montage") , title("Cr");
 
 end
 
+
+%sharpening funzia?
