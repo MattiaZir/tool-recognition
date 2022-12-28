be1f8ab6dcdb60d458326e5fee23026200b66f28
Alessandro <a.preziosa1@campus.unimib.it>
2022-12-28 11:32:35 +0000

    mask + precise per finestra + piccola

--- createGTmasks.m
+++ createGTmasks.m
@@ -4,13 +4,37 @@ addpath(genpath('support/'));
 [images, labels] = readlists();
 n = numel(images);
 
-for i = 41: n
-    im = im2double(imread(strcat("data/",images{i})));
-    im = im(:,:,1);
+for i = 46:n
+    im = im2double(rgb2gray(imread(strcat("data/",images{i}))));
     
-    bw = 1 - sauvola(im,[150 150]);%strategia migliore
-    bw = imclose(bw, strel('disk', 10));
-    bw = imopen(bw, strel('disk', 5));
-    figure, imshowpair(im, bw, 'montage');
+    im = medfilt2(im, [10 10]);
+    im = imfilter(im, fspecial("gaussian", 10, 1.8));
+
+    [r c ch] = size(im);
     
+    res = compute_local_descriptors(im, 20, 7, @compute_lbp);%prima era 30 15
+    
+    labels = kmeans(res.descriptors, 2);
+    counts = histcounts(labels);
+
+
+    labels_image = reshape(labels, [res.nt_rows res.nt_cols]);%la pic della label è 5 volte più piccola di raw
+    labels_image = imresize(labels_image, [r,c], "nearest");%uso Nneighbor per non fargli generare nuovi valori di labels
+    
+    if(counts(1)>counts(2)) %cerco classe + numerosa (lo sfondo)
+        bw = labels_image==2;
+    else 
+        bw = labels_image==1;
+    end
+
+%     figure, subplot(1,4,1), imshow(im);
+%     subplot(1,4,2), imshow(bw);
+    %bw = imclose(bw, strel('disk',5));
+    bw = imopen(bw, strel('disk',10));
+    %bw = imclose(bw, strel('disk',20));
+%     subplot(1,4,3), imshow(bw);
+%     subplot(1,4,4), imshow(edge(im, 'sobel'));
+
+   imwrite(uint8(bw.*255), strcat("data/annotaz/", pad(string(i),4,'left','0'),'.png'));
 end
+
