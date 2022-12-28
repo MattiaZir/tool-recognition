16f786e848f6c61730524d8e0e35d68352989a33
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2022-12-28 21:31:20 +0100

    aggiornato compareMasks

--- compareMasks.m
+++ compareMasks.m
@@ -3,7 +3,7 @@ close all;
 addpath(genpath('support/'));
 [images, labels] = readlists();
 n = numel(images);
-results = zeros(n, 1);
+res_table = cell2table(cell(0,3), 'VariableNames', {'#', 'label', 'errors'});
 
 for k = 1:n
     % Essendo le immagini in png, devo cambiare la stringa con strrep
@@ -11,12 +11,33 @@ for k = 1:n
     gt_im = imread(strcat("data/annotaz/", im_name)) > 0;
     gen_im = imread(strcat("data/generated/", im_name)) > 0;
 
-
     image_diff = imabsdiff(gt_im, gen_im);
-    results(k) = sum(image_diff(:));
+    diff_sum = sum(image_diff(:));
+
+    tmp = {k, labels{k}, diff_sum};
+
+
+    res_table = [res_table; tmp];
 end
 
-figure, bar(1:n, results);
+%% Creazione dei grafici
+figure, bar(res_table.("#"), res_table.errors);
 title("Errori maschera");
 xlabel("# Immagine");
 ylabel("# Errori");
+
+% Raggruppo le statistiche
+grouped = grpstats(res_table, "label", ["sum","min", "max", "mean", "std"]);
+
+% ERRORI TOTALI
+figure, bar(categorical(grouped.label), grouped.sum_errors);
+title("Totale errori per label");
+xlabel("Label");
+ylabel("# Errori");
+
+% MEDIA ERRORI
+figure, bar(categorical(grouped.label), grouped.mean_errors);
+title("Media errori per label");
+xlabel("Label");
+ylabel("# Errori");
+
