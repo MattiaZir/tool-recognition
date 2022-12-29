76a1815e7a491615cfcfccbb9b5905c2dd41f5f4
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2022-12-29 16:05:11 +0100

    esperimento tophat

--- esperimenti/morphologySegm/morfologia_bin_otsu.m
+++ esperimenti/morphologySegm/morfologia_bin_otsu.m
@@ -3,21 +3,34 @@ close all;
 addpath(genpath('support/'));
 [images, l] = readlists();
 n = numel(images);
-mkdir(".\otsu_bin_bhat_30");
+mkdir(".\otsu_bin_that_30");
 
+%% BOTTOM HAT
+% for j = 1 : n
+%     im = im2double(rgb2gray(imread(images{j})));
+% 
+%     se = strel("disk", 30);
+%     closed = imclose(im, se);
+%     bottomhat = closed - im;
+%     bw = imbinarize(bottomhat);
+% 
+%     im = insertText(im, [50 50], "originale");
+%     closed = insertText(closed, [30 30], "Chiusura con disco r=30");
+%     bottomhat = insertText(bottomhat, [30 30], "bottomhat");
+%     
+%     figure("Visible","off"), imshow(bw);
+%     saveas(gcf, "./otsu_bin_bhat_30/" + j, 'png');
+% end
 
+%% TOPHAT
 for j = 1 : n
     im = im2double(rgb2gray(imread(images{j})));
 
     se = strel("disk", 30);
-    closed = imclose(im, se);
-    bottomhat = closed - im;
-    bw = imbinarize(bottomhat);
-
-    im = insertText(im, [50 50], "originale");
-    closed = insertText(closed, [30 30], "Chiusura con disco r=30");
-    bottomhat = insertText(bottomhat, [30 30], "bottomhat");
+    opened = imopen(im, se);
+    tophat = im - opened;
+    bw = imbinarize(tophat);
     
     figure("Visible","off"), imshow(bw);
-    saveas(gcf, "./otsu_bin_bhat_30/" + j, 'png');
+    saveas(gcf, "./otsu_bin_that_30/" + j, 'png');
 end
