82d116363da5d323e4428dab28a3e8f609651ac4
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2022-12-29 16:59:44 +0100

    esperimenti 5 e 6

---
+++ esperimenti/morphologySegm/Copy_of_morph_bin_otsu_smooth.m
@@ -0,0 +1,23 @@
+clear;
+close all;
+addpath(genpath('support/'));
+[images, l] = readlists();
+n = numel(images);
+mkdir(".\otsu_bin_sm_morph_sm_3_op_9");
+
+
+for j = 1 : 3 : n
+    im = im2double(rgb2gray(imread(images{j})));
+
+    se = strel("disk", 3);
+    dil = imdilate(im, se);
+    er = imerode(im, se);
+    bw = imbinarize(dil - er);
+    
+    se2 = strel("disk", 9);
+    bw_opened = imopen(bw, se2);
+
+    figure("Visible","off"), imshow(bw_opened);
+
+    saveas(gcf, "./otsu_bin_sm_morph_sm_3_op_9/" + j, 'png');
+end
