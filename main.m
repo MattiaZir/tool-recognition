fce3fe34d87a9adc55f0edc1cf766a76f9ce2aa9
Alessandro <a.preziosa1@campus.unimib.it>
2023-01-01 21:32:10 +0000

    .list files aggiornati + prove su queste pic con gradiente in main.m

--- main.m
+++ main.m
@@ -5,41 +5,38 @@ addpath(genpath('support/'));
 n = numel(images); 
 
 
-for i = 1 : 1 % solo 1 pic ha la gt quindi... 1:1 e non 1:n
+for i = n-20 : n % solo 1 pic ha la gt quindi... 1:1 e non 1:n
     im = imread(images{i});
-    im = imresize(im, [768, 1024]);
+    im = imresize(im, [768, 1024]);%le mie pic sono in 4/3 altre no
     im = im2double(rgb2gray(im));
     im = medfilt2(im, [10 10]);
+    im = medfilt2(im, [10 10]);
+    im = medfilt2(im, [10 10]);
+    im = imfilter(im, fspecial("gaussian", 10, 1.8));
+    im = imfilter(im, fspecial("gaussian", 10, 1.8));
     im = imfilter(im, fspecial("gaussian", 10, 1.8));
 
     
+%     figure, imshow(im);
     bw = segmenta(im);
     
-    figure, imshow(bw);
+%     figure, imshow(bw);
 
+    edg = edge(im, 'sobel');
+
+    figure, imshow(edg);%gli edge li trova giusti, ma come sfruttare questa info?
+    edg = imdilate(edg, strel('disk', 30));
+    im2 = im .* edg;
+
+%     figure, imshow(im2);
 
 %     figure, imshow(im);
 %     bw = imopen(bw, strel('disk',5));
 %     bw = imclose(bw, strel('disk',10));
     
 
+%     L = labelingCompConn(bw);
+%     figure, imagesc(L), title("labellata");
 
-    [L,N] = bwlabel(bw,8);
-    numPixelsPerCC = histcounts(L);%alla posiz (1) c'è il count di pixel dell'obj 0!! non 1
-
-    for j = 1:N %numPixelsPerCC(i) ha i pixel dell'obj i+1, perchè a posiz 1 ha quelli dello sfondo!
-        if numPixelsPerCC(j+1) < 450 %dipende dalla size
-            mask = L==j;
-            mask = mask.*j;
-%             figure(j), imshow(mask.*36);
-            [rows,cols] = find(mask);%coordinates for the pixels in object j
-            L = L - mask;
-        end
-    end
-    
-    
-    
-    L = imclearborder(L,4);%tolgo gli obj che toccano i bordi
-    figure, imshow(L);
 
 end
