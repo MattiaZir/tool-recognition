a70991c475349494f1fa5c2ca40a859c42c38861
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2023-01-02 17:40:17 +0100

    esperimenti falliti.

---
+++ esperimenti/superpixelSegm/esperimenti.md
@@ -0,0 +1,10 @@
+# ESPERIMENTO 1
+
+Riduzione progressiva dei superpixel
+
+## PARAMETRI
+Superpixels: 1000 -> 800
+
+## OSSERVAZIONI
+
+Promette bene, ma non abbastanza
---
+++ esperimenti/superpixelSegm/esperimento_1/esperimento1.m
@@ -0,0 +1,45 @@
+clear;
+close all;
+addpath(genpath('support/'));
+[images, l] = readlists();
+n = numel(images);
+mkdir(".\esperimento_1");
+
+
+for j = 1 : 10 : n
+    im = im2double(imread(images{j}));
+    results = ones(size(im, 1), size(im, 2));
+    [L,numLabels] = superpixels(im,1000);
+    [r, c, ch] = size(im);
+    
+
+    outputImage = zeros(size(im),'like',im);
+    idx = label2idx(L);
+
+    for labelVal = 1:numLabels
+        redIdx = idx{labelVal};
+        greenIdx = idx{labelVal}+r*c;
+        blueIdx = idx{labelVal}+2*r*c;
+
+        outputImage(redIdx) = mean(im(redIdx));
+        outputImage(greenIdx) = mean(im(greenIdx));
+        outputImage(blueIdx) = mean(im(blueIdx));
+    end  
+
+    [L,numLabels] = superpixels(outputImage, 800);
+    idx = label2idx(L);
+
+    for labelVal = 1:numLabels
+        redIdx = idx{labelVal};
+        greenIdx = idx{labelVal}+r*c;
+        blueIdx = idx{labelVal}+2*r*c;
+
+        outputImage(redIdx) = mean(im(redIdx));
+        outputImage(greenIdx) = mean(im(greenIdx));
+        outputImage(blueIdx) = mean(im(blueIdx));
+    end  
+
+
+    figure("Visible","off"), imshow(outputImage);
+    saveas(gcf, "./esperimento_1/" + j, 'png');
+end
