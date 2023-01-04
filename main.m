8c0e3df289b69d7e4d99eaaa87d065ec852fb7d9
Alessandro <a.preziosa1@campus.unimib.it>
2023-01-04 10:25:46 +0000

    pic 72 + prove segm by classification

--- main.m
+++ main.m
@@ -3,44 +3,56 @@ close all;
 addpath(genpath('support/'));
 [images, dummy, paths2gt, labels_meaning] = readlists();
 n = numel(images); 
+%classificatore knn per trovare i pixel di una immagine che sono di obj
 
+train.values=[];
+train.labels=[];
 
-for i = n-15 : n % solo 1 pic ha la gt quindi... 1:1 e non 1:n
-    im = imread(images{i});
-    im = imresize(im, [768, 1024]);%le mie pic sono in 4/3 altre no
-    im = im2double(rgb2gray(im));
-    im = medfilt2(im, [10 10]);
-    im = medfilt2(im, [10 10]);
-    im = medfilt2(im, [10 10]);
-    im = medfilt2(im, [10 10]);
-    im = medfilt2(im, [10 10]);
-    im = imfilter(im, fspecial("gaussian", 10, 1.8));
-    im = imfilter(im, fspecial("gaussian", 10, 1.8));
-    im = imfilter(im, fspecial("gaussian", 10, 1.8));
-    im = imfilter(im, fspecial("gaussian", 10, 1.8));
-    im = imfilter(im, fspecial("gaussian", 10, 1.8));
- 
+for i = 1 : 5: 71
+    im = im2double(imread(images{i}));
+    im = rgb2gray(imresize(im, 0.3));%le mie pic sono in 4/3 altre no
+    gt = imread(paths2gt{i});
+    gt = imresize(gt(:,:,1), 0.3)>0;    
+
+    [r c ~] = size(im);
+
+    res = compute_local_descriptors(im, 20, 10, @compute_lbp);
     
-%     figure, imshow(im);
-    bw = segmenta(im);
+    lbpAGrandezzaGiusta = reshape(res.descriptors, [res.nt_rows res.nt_cols]);%la pic è t_step volte(forse) più piccola di raw
+    lbpAGrandezzaGiusta = imresize(lbpAGrandezzaGiusta, [r,c], "nearest");%uso Nneighbor per non fargli generare nuovi valori 
     
-%     figure, imshow(bw);
-
-    edg = edge(im, 'sobel');
-
-    figure, imshow(edg);%gli edge li trova giusti, ma come sfruttare questa info?
-    edg = imdilate(edg, strel('disk', 30));
-    im2 = im .* edg;
-
-%     figure, imshow(im2);
-
-%     figure, imshow(im);
-%     bw = imopen(bw, strel('disk',5));
-%     bw = imclose(bw, strel('disk',10));
+    skin = lbpAGrandezzaGiusta .* gt;
+    noskin = lbpAGrandezzaGiusta .* (1-gt);
+    [rs,cs,ch] = size(skin);
+    skin = reshape(skin, rs*cs, ch);
     
-
-%     L = labelingCompConn(bw);
-%     figure, imagesc(L), title("labellata");
-
+    [rns,cns,ch] = size(noskin);
+    noskin = reshape(noskin, rns*cns, ch);
+    %todo: migliorare efficienza
+    train.values = [train.values; noskin; skin];%etichetta 0 a noskin e 1 a skin ---v
+    train.labels = uint8([train.labels; zeros(rns*cns, 1); ones(rs*cs, 1)]);%nelle prime n righe ho i dati skin e dopo i noskin
 
 end
+
+
+%vuole i dati per riga per classificarli
+classifier = fitcknn(train.values, train.labels, "NumNeighbors", 3);%k=1 di base 
+
+%lo uso sul test set ora
+
+im = im2double(imread("data/0072.jpg"));
+[ri, ci, ch] = size(im);
+test.values = reshape( im, ri*ci, ch);
+
+gt = imread(paths2gt{72});
+gt = gt(:,:,1) >0;
+test.labels = uint8(reshape( gt, ri*ci, 1)) ;
+
+predicted = predict(classifier, test.values);%vettore di label: 0 e 1 (ho due classi)
+
+cm = confmat(test.labels, predicted);%confronto predizioni-gtruth
+figure, show_confmat(cm.cm_raw, ["noskin", "skin"]);
+
+
+p = reshape(predicted, ri, ci, 1)>0;%>0 per il cast in booleani
+show_result(im, p);
