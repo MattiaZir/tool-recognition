a5893db5b546e5bf92e48f06437ed9efb047ea25
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2023-01-07 19:55:18 +0100

    labelingCompConnesse con bwareaopen

---
+++ esperimenti/classification/labelingCompConn.m
@@ -0,0 +1,24 @@
+function out = labelingCompConn(bw)
+
+    
+    [L,N] = bwlabel(bwareaopen(bw, 50),8);
+%     numPixelsPerCC = histcounts(L);%alla posiz (1) c'è il count di pixel dell'obj 0!! non 1
+% 
+%     %tolgo gli obj piccoli
+%     for j = 1:N %numPixelsPerCC(i) ha i pixel dell'obj i+1, perchè a posiz 1 ha quelli dello sfondo!
+%         if numPixelsPerCC(j+1) < 50 %dipende dalla size TODO MIGLIORA
+%             mask = L==j;
+%             mask = mask.*j;
+% %             figure(j), imshow(mask.*36);
+%             [rows,cols] = find(mask);%coordinates for the pixels in object j
+%             L = L - mask;
+%         end
+%     end
+    
+    
+    
+    %L = imclearborder(L,4);%tolgo gli obj che toccano i bordi
+%     figure, imshow(L);
+    out = L;
+
+end
