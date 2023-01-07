ca60e299d6fbe75c52ca5d13a7bbb8d2dc0d781c
Alessandro <a.preziosa1@campus.unimib.it>
2023-01-07 20:59:25 +0000

    prove orientamento

--- main.m
+++ main.m
@@ -6,10 +6,11 @@ n = numel(images);
 
 for i =  8 : n %test set
     im = im2double(imread(images{i}));
+    figure, subplot(2,2,1), imshow(im);
     im = rgb2gray(imresize(im, [154 205],"nearest"));
-
-    bw = segmentaViaClassificazione(im);
-    cc = labelingCompConn(bw);
-    figure(i), subplot(2,1,2),imagesc(cc);
-    subplot(2,1,1), imshow(im);
+    subplot(2,2,4), imshow(im);
+%     bw = segmentaViaClassificazione(im);
+%     cc = labelingCompConn(bw);
+%     figure(i), subplot(2,1,2),imagesc(cc);
+%     subplot(2,1,1), imshow(im);
 end
