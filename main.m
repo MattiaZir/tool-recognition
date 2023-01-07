f0d259bf14c4e3867bfa5e8303ea25bec1f44e79
Alessandro <a.preziosa1@campus.unimib.it>
2023-01-07 19:53:02 +0000

    bwarea + imopen

--- main.m
+++ main.m
@@ -4,11 +4,13 @@ addpath(genpath('support/'));
 [images, symbolicLabels, paths2gt, labels_meaning] = readlists('multiple');
 n = numel(images); 
 
-for i =  1 : n %test set
+for i =  8 : 18 %test set
     im = im2double(imread(images{i}));
     im = rgb2gray(imresize(im, [154 205],"nearest"));
 
     bw = segmentaViaClassificazione(im);
-    cc = labelingCompConn(bw);
+    cc = labelingCompConn(bw, 0);
+    figure, imagesc(cc);
+    cc = labelingCompConn(bw, 1);
     figure, imagesc(cc);
 end
