2264f172f5b96a0ec8f96e6aef14e63fa9d90e1c
Alessandro <a.preziosa1@campus.unimib.it>
2023-01-07 21:05:34 +0000

    main definitivo

--- main.m
+++ main.m
@@ -6,11 +6,8 @@ n = numel(images);
 
 for i =  8 : n %test set
     im = im2double(imread(images{i}));
-    figure, subplot(2,2,1), imshow(im);
     im = rgb2gray(imresize(im, [154 205],"nearest"));
-    subplot(2,2,4), imshow(im);
-%     bw = segmentaViaClassificazione(im);
-%     cc = labelingCompConn(bw);
-%     figure(i), subplot(2,1,2),imagesc(cc);
-%     subplot(2,1,1), imshow(im);
+    bw = segmentaViaClassificazione(im);
+    cc = labelingCompConn(bw);
+    figure, imagesc(cc);
 end
