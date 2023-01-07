92b88ac0e66fcb496a7bb822d0a6c202c6a97d7b
Alessandro <a.preziosa1@campus.unimib.it>
2023-01-07 20:35:10 +0000

    labelling fatto bene main in progresso

--- labelingCompConn.m
+++ labelingCompConn.m
@@ -1,14 +1,10 @@
-function out = labelingCompConn(bw, i)
-    if mod(i,2)==0
-        bw = bwareaopen(bw, 50); %removes all connected components (objects) that have fewer than P pixels(2nd parameter)
-        bw = imclose(bw, strel('disk', 4));
-    end
-    [L,N] = bwlabel(bw,8);
-    numPixelsPerCC = histcounts(L);%alla posiz (1) c'è il count di pixel dell'obj 0!! non 1
-
-           
+function out = labelingCompConn(bw)
+    bw = imclose(bw, strel('disk', 3));
+    bw = bwareaopen(bw, 50); %removes all connected components (objects) that have fewer than P pixels(2nd parameter)        
+    
+    
+    [L,N] = bwlabel(bw,8);%labelling vero e proprio
     L = imclearborder(L,4);%tolgo gli obj che toccano i bordi
-
     out = L;
 
 end
--- main.m
+++ main.m
@@ -4,13 +4,12 @@ addpath(genpath('support/'));
 [images, symbolicLabels, paths2gt, labels_meaning] = readlists('multiple');
 n = numel(images); 
 
-for i =  8 : 18 %test set
+for i =  8 : n %test set
     im = im2double(imread(images{i}));
     im = rgb2gray(imresize(im, [154 205],"nearest"));
 
     bw = segmentaViaClassificazione(im);
-    cc = labelingCompConn(bw, 0);
-    figure, imagesc(cc);
-    cc = labelingCompConn(bw, 1);
-    figure, imagesc(cc);
+    cc = labelingCompConn(bw);
+    figure(i), subplot(2,1,2),imagesc(cc);
+    subplot(2,1,1), imshow(im);
 end
