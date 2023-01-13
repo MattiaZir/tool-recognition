5dbca93af557c0b5b0299568f39f62439d5a7fcc
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2023-01-13 17:49:44 +0100

    Sistemate immagini classificazione

--- main.m
+++ main.m
@@ -3,15 +3,16 @@ close all;
 addpath(genpath('support/'));
 [images, symbolicLabels, paths2gt, labels_meaning] = readlists('multiple');
 n = numel(images); 
+scale_res = [154 205];
 
 for i =  8 : n %test set
     im = im2double(imread(images{i}));
-    im = rgb2gray(imresize(im, [154 205],"nearest"));
-    bw = segmentaViaClassificazione(im);
+    img = rgb2gray(imresize(im, scale_res, "nearest"));
+    bw = segmentaViaClassificazione(img);
     cc = labelingCompConn(bw);
     [bBoxLocs, lbls] = classify(cc);
     
-    fig = insertObjectAnnotation(im, "rectangle", bBoxLocs, lbls);
-
+    % Questo crea l'immagine con le dimensioni originali e a colori
+    fig = createAnnotatedImage(im, scale_res, bBoxLocs, lbls);
     figure, imshow(fig);
 end
---
+++ support/createAnnotatedImage.m
@@ -0,0 +1,5 @@
+function fig = createAnnotatedImage(im, scale_res, bBoxLocs, lbls)
+    scale_fac = size(im, 1:2)./scale_res;
+    bBoxLocs = bBoxLocs.*repmat(scale_fac, [1 2]);
+    fig = insertObjectAnnotation(im, "rectangle", bBoxLocs, lbls);
+end
