ae6248c552a50ac1cbaceb47f9b807cab4c35906
Mattia Zirpoli <5838871+MattiaZir@users.noreply.github.com>
2023-01-18 18:21:37 +0100

    Update classificaOggetti.m

--- classificaOggetti.m
+++ classificaOggetti.m
@@ -8,7 +8,7 @@ function [bBox, objs] = classificaOggetti(cc, labels_meaning)
     bBox = [];
     objs = [];
     
-    tmp = zeros(size(cc))%pic grande come l'originale in cui metterò 1 dove riconosco una forbice 2 dove riconosco un metro ...
+    tmp = zeros(size(cc));%pic grande come l'originale in cui metterò 1 dove riconosco una forbice 2 dove riconosco un metro ...
 
     for i = 1:length(cc_unique) % parte da 2 perché 1 corrisponde allo sfondo ??????????
         regione = cc==i;
@@ -16,7 +16,7 @@ function [bBox, objs] = classificaOggetti(cc, labels_meaning)
         [label, prob] = predict(classifierOggetti, splitvars(T));
     
         if(max(prob) < labelT)
-            label = 11 % "unknown"
+            label = 11; % "unknown"
         end
     
         % proprietà per il labeling sull'immagine
